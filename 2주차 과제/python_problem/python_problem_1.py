6564c5c8d95e98321316238b3e44ebb0976e645b
sookyung <thoosoo@naver.com>
2022-07-08 07:42:35 +0900

    1-3단계

--- 2주차 과제/python_problem/python_problem_1.py
+++ 2주차 과제/python_problem/python_problem_1.py
@@ -4,5 +4,16 @@ from subprocess import call
 
 num = 0
 
+ 
+while True:
 
-call = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+    try:
+        call = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+    except:
+        print('정수를 입력하세요')
+    else:
+        if call < 1 or call > 3:
+            print('1,2,3 중 하나를 입력하세요')
+        else:
+            break
+            
