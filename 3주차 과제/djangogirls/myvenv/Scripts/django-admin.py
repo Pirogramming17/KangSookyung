27193e5dce609731a1a0454429b68d4fc3a255df
sookyung <thoosoo@naver.com>
2022-07-13 14:11:25 +0900

    장고뷰 만들기

---
+++ 3주차 과제/djangogirls/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\강수경\newfile\kangsookyung\3주차 과제\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
